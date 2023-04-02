04c1cf05ee7495e3b2efe0c3142a19bf45c359a3
jjdeharo <jjdeharo@gmail.com>
2023-04-02 07:40:22 +0200

    Actualización:

---
+++ git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Solicita una descripción para el commit
+read -p "Ingrese una descripción para el commit: " descripcion
+
+# Si el usuario no ingresó nada, establece el texto por defecto
+if [ -z "$descripcion" ]
+then
+  descripcion="Actualización:"
+fi
+
+# Agrega los cambios
+git add .
+
+# Hace el commit con la descripción ingresada por el usuario o la predeterminada
+git commit -m "$descripcion"
+
+# Sube los cambios al repositorio remoto
+git push
